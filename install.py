2558f73ee87efbeb99a907b927d4afe0ac42d885
Marcin Rim <mrim@neubloc.com>
2011-09-02 14:05:23 +0200

    install file

---
+++ install.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import os, sys
+import fileinput
+
+if os.getuid() != 0:
+    print "You need to run this as root"
+    sys.exit()
+
+cwd = os.getcwd()
+print "Creating symlink radio-gtk",
+os.unlink('/usr/local/bin/radio-gtk')
+os.symlink("%s/main.py" % cwd, '/usr/local/bin/radio-gtk')
+print "..done"
+
+print "Changind working dir in main.py",
+for line in fileinput.input("main.py", inplace=1):
+    if line.find('os.chdir') == 0: 
+        print  "os.chdir('%s')" % (cwd)
+    else:
+        print line,
+print "..done"
